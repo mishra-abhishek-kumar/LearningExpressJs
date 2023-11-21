10f4afdfe14aefa8bbb73c6e8824ee5813557516
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2023-11-21 11:25:23 +0530

    Sending file as response

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send("Hello User");
 });
 
+//sending file as response
+app.get('/file', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
 app.listen(4000, () => {
     console.log("Listening on port 4000");
 })
